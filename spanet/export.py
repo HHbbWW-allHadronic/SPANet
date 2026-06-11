ac3a2f2190db396c6daa305c15e33d82f5879f66
Garath Vetters <gavetter@ttu.edu>
2026-06-11 10:58:04 +0000

    added checkpoint selection to export.py

--- spanet/export.py
+++ spanet/export.py
@@ -125,12 +125,14 @@ def main(
         output_embeddings: bool,
         gpu: bool,
         opset: int
+        checkpoint: str = None
 ):
     major_version, minor_version, *_ = torch.__version__.split(".")
     if int(major_version) == 2 and int(minor_version) == 0:
         raise RuntimeError("ONNX export with Torch 2.0.x is not working. Either install 2.1 or 1.13.")
 
-    model = load_model(log_directory, cuda=gpu)
+    #model = load_model(log_directory, cuda=gpu)
+    model = load_model(log_directory, cuda=gpu, checkpoint=checkpoint)
 
     # Create wrapped model with flat inputs and outputs
     wrapped_model = WrappedModel(model, input_log_transform, output_log_transform, output_embeddings)
@@ -185,6 +187,10 @@ if __name__ == '__main__':
 
     parser.add_argument("--output-embeddings", action="store_true",
                         help="Exported model will also output the embeddings for every part of the event.")
+
+    parser.add_argument("-c", "--checkpoint", type=str, default=None,
+                    help="Path to a specific checkpoint file to export. "
+                         "If not provided, uses the last checkpoint from the log directory.")
 
     arguments = parser.parse_args()
     main(**arguments.__dict__)
